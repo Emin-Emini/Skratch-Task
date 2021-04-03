0df2a855a01b21d704c0b0c27cfedf49cabfd13b
EminRma <eminrmae@gmail.com>
2021-04-03 14:53:34 +0200

    Setting Up: TableView Data Source and Delegate

--- Skratch Task/ViewController/FriendsViewController.swift
+++ Skratch Task/ViewController/FriendsViewController.swift
@@ -12,10 +12,25 @@ class FriendsViewController: UIViewController {
     //MARK: - Outlets
     @IBOutlet weak var friendsTableView: UITableView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        friendsTableView.delegate = self
+        friendsTableView.dataSource = self
     }
 
+}
+
+//MARK: - Table View
+extension FriendsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as! FriendTableViewCell
+        
+        return cell
+    }
 }
